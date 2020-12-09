8563908b73cf34940eb531524f24429f22ba18af
dawooonkim <dawoonkim0407@gmail.com>
2020-12-10 01:27:11 +0900

    refact:add-return 함수 정리
    - 소스코드 리팩토링

--- js-basic-book-master/05/js/add-return.js
+++ js-basic-book-master/05/js/add-return.js
@@ -1,32 +1,41 @@
-function addReturn(){
-    function real(){
-        var trueNum = parseInt(prompt("첫번째 숫자는?"));
-        if (trueNum > 0){
+function sum(){
+
+    // 인수가 숫자인지 확인하는 함수
+    function real(a){
+        if (a > 0){
+            var trueNum = a;
             return trueNum;
         } else {
-            var trueNum = false;
-            return trueNum;
+            return false;
         }
     }
 
+    // 인수끼리 더하는 함수
     function addNumber(a, b){
         return a + b;
     }
 
+    var q1 = "첫번째 숫자를 입력해주세요.";
+    var q2 = "두번째 숫자를 입력해주세요.";
+    var a1 = "숫자를 입력해주세요."
+
     // trueNum을 반환하여 num1을 받아옴
-    var num1 = real();
-    // num1 이 제대로 입력되지 않았을때 재실행
+    var num1 = real(parseInt(prompt(q1)));
     while (num1 === false) {
-        alert("숫자를 입력해주세요.");
-        var num1 = real();
+        alert(a1);
+        var num1 = real(parseInt(prompt(q1)));
         continue;
     }
 
-
-    // num2를 입력받음
-    var num2 = parseInt(prompt("두번째 숫자는?"));
+    // trueNum을 반환하여 num2을 받아옴
+    var num2 = real(parseInt(prompt(q2)));
+    while (num2 === false) {
+        alert(a1);
+        var num2 = real(parseInt(prompt(q2)));
+        continue;
+    }
 
     // num1과 num2를 더하여 보여줌
     var result = addNumber(num1, num2);
-    alert("두 수의 합은" + result + "입니다");
+    alert("두 수의 합은 " + result + "입니다.");
 }
