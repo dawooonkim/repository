e5b4542e0e3e62422340442db2c6e3d99a138d82
dawooonkim <dawoonkim0407@gmail.com>
2020-12-19 00:55:01 +0900

    feat: Array practice v3

--- js-basic-book-master/07/js/checklist.js
+++ js-basic-book-master/07/js/checklist.js
@@ -1,6 +1,20 @@
+
 var itemList = [];
 var addBtn = document.querySelector("#add");
+var myInput = document.querySelector("#item");
+var form = document.querySelector("#form");
+
 addBtn.addEventListener("click", addList);
+myInput.addEventListener("keyup",function(e){
+    if(e.keyCode === 13){
+        addList(); // enter key 입력시 버튼 클릭과 동일하게 동작
+    }
+});
+form.addEventListener("submit", function(e){
+    e.preventDefault(); // submit reload 막기
+});
+
+
 function addList(){
     var item = document.querySelector('#item').value;
     if(item != "") {
@@ -10,11 +24,22 @@ function addList(){
     }
     showList();
 }
+
 function showList() {
     var list = "<ul>";
     for (var i = 0; i < itemList.length; i++){
-        list += "<li>" + itemList[i] + "</li>";
+        list += "<li>" + itemList[i] + "<span class='close' id=" + i + ">X</span></li>";
     }
     list += "</ul>";
     document.querySelector('#itemList').innerHTML = list;
+    var remove = document.querySelectorAll('.close');
+    for(var i = 0; i<remove.length; i++){
+        remove[i].addEventListener("click", removeList);
+    }
+}
+
+function removeList() {
+    var id = this.getAttribute("id");
+    itemList.splice(id, 1);
+    showList();
 }
