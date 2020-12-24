471c618a0ca4da9a8d051d4c2a67f98a52864cf6
dawooonkim <dawoonkim0407@gmail.com>
2020-12-24 23:35:45 +0900

    feat: DOM practice v2

--- js-basic-book-master/08/js/product.js
+++ js-basic-book-master/08/js/product.js
@@ -1,5 +1,6 @@
-		var bigPic = document.querySelector("#cup");  
-		var smallPics = document.querySelectorAll(".small");  
+		var isOpen = false;
+		var bigPic = document.querySelector("#cup");
+		var smallPics = document.querySelectorAll(".small");
 
 		for(i=0; i<smallPics.length; i++) {
 			smallPics[i].addEventListener("click", function() {
@@ -7,3 +8,16 @@
 				bigPic.setAttribute("src", newPic);
 			});
 		}
+
+		var view = document.querySelector('#view');
+		view.addEventListener("click", function(){
+			if(isOpen === false) {
+				document.querySelector('#detail').style.display = "block";
+				view.innerText = "닫기";
+				isOpen = true;
+			} else {
+				document.querySelector('#detail').style.display = "none";
+				view.innerText = "상세 설명 열기";
+				isOpen = false;
+			}
+		})
