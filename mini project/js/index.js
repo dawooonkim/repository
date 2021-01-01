52b4541124669999be3bd19d28427e541c85e9a3
dawooonkim <dawoonkim0407@gmail.com>
2021-01-02 00:33:17 +0900

    mini-1 feat: nextQuestion 변수 추가

--- mini project/js/index.js
+++ mini project/js/index.js
@@ -1,10 +1,26 @@
 var quiz = document.querySelectorAll("[class^='session']");
 var answer = document.querySelectorAll('.answer');
+var init = quiz[0];
+var n = 0;
+var next;
+var now;
+
 
 quiz[0].style.display = 'block';
+for(var i = 0; i<answer.length; i++){
+    answer[i].addEventListener('click', function(){
+        n = n + 1;
+        next = quiz[0+n];
+        now = quiz[n-1];
+        nextQuestion();
+    })
+}
 
+function nextQuestion(){
+    now.style.display = 'none';
+    next.style.display = 'block';
+}
 
-// se1.style.display = 'block';
 // answer[0].addEventListener('click', function(){
 //     se1.style.display = 'none';
 //     se2.style.display = 'block';
